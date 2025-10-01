2969658458ed28213ff80ec2e2890dc0deb49cee
Bogdanpups1 <kapishevskii11@gmail.com>
2025-10-01 09:37:19 +0300

    1234

--- MyString/main.cpp
+++ MyString/main.cpp
@@ -7,19 +7,19 @@ int main()
     MyString obj("Hello");
     obj.Print();
 
-    cout << "Search for 'ello': " << obj.MyStrStr("ello") << endl;
+    /*cout << "Search for 'ello': " << obj.MyStrStr("ello") << endl;
     cout << "Search for 'xyz': " << obj.MyStrStr("xyz") << endl;
 
     cout << "Search for 'e': " << obj.MyChr('e') << endl;
     cout << "Search for 'z': " << obj.MyChr('z') << endl;
 
-    cout << "Length: " << obj.MyStrLen() << endl;
+    cout << "Length: " << obj.MyStrLen() << endl;*/
 
-    MyString obj2(" World!");
-    obj.MyStrCat(obj2);
-    obj.Print();
+    MyString obj2(" World");
+    /*obj.MyStrCat(obj2);*/
+    obj2.Print();
 
-    obj.MyDelChr('o');
+    /*obj.MyDelChr('o');
     cout << "After deleting 'o': ";
     obj.Print();
 
@@ -29,5 +29,16 @@ int main()
 
     cout << "Initial count: " << MyString::GetCount() << endl;
 
-    return 0;
+    return 0;*/
+
+    MyString rez = obj + obj2;
+    rez.Print();
+
+    MyString rez2 = obj + "!!!";
+    rez2.Print();
+
+    MyString rez3 = obj + '?';
+    rez3.Print();
+
+    //MyString rez3 = obj - "world"; // Hello world - world = Hello
 }
