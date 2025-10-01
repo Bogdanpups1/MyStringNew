85e0b7bb237b6ba1524c334d1057aaf4842655c2
Bogdanpups1 <kapishevskii11@gmail.com>
2025-10-01 09:36:29 +0300

    1234

--- MyString/MyString.h
+++ MyString/MyString.h
@@ -15,13 +15,16 @@ public:
 	void Print();
 	//--------------------------------------------------
 	void Input();
-	bool MyStrStr(const char* st);// поиск подстроки в строке
-	int MyChr(char c); // поиск символа в строке (индекс найденного символа,либо - 1)
-	int MyStrLen();// возвращает длину строки
-	void MyStrCat(MyString& b); // объединение строк
-	void MyDelChr(char c); // удаляет указанный символ
-	int MyStrCmp(MyString& b); // сравнение строк
+	bool MyStrStr(const char* st);// РїРѕРёСЃРє РїРѕРґСЃС‚СЂРѕРєРё РІ СЃС‚СЂРѕРєРµ
+	int MyChr(char c); // РїРѕРёСЃРє СЃРёРјРІРѕР»Р° РІ СЃС‚СЂРѕРєРµ (РёРЅРґРµРєСЃ РЅР°Р№РґРµРЅРЅРѕРіРѕ СЃРёРјРІРѕР»Р°,Р»РёР±Рѕ - 1)
+	int MyStrLen();// РІРѕР·РІСЂР°С‰Р°РµС‚ РґР»РёРЅСѓ СЃС‚СЂРѕРєРё
+	void MyStrCat(MyString& b); // РѕР±СЉРµРґРёРЅРµРЅРёРµ СЃС‚СЂРѕРє
+	void MyDelChr(char c); // СѓРґР°Р»СЏРµС‚ СѓРєР°Р·Р°РЅРЅС‹Р№ СЃРёРјРІРѕР»
+	int MyStrCmp(MyString& b); // СЃСЂР°РІРЅРµРЅРёРµ СЃС‚СЂРѕРє
 
 	static int GetCount();
 
+	MyString operator+ (MyString& b);
+	MyString operator+ (const char* b);
+	MyString operator+ (char c);
 };
